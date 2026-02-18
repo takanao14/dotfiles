69a7a6d82229917b8caccd70086729ec0c506f02
takanao <takanao14@gmail.com>
2026-02-18 19:24:59 +0900

    add: refactor fpath.zsh to conditionally set FPATH based on brew installation

--- dot_zsh.d/source/fpath.zsh
+++ dot_zsh.d/source/fpath.zsh
@@ -1,2 +1,12 @@
-FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
-FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+if command -v brew &> /dev/null; then
+    if [[ ":$FPATH:" != *":$(brew --prefix)/share/zsh/site-functions:"* ]]; then
+        FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+    fi
+    if [[ ":$FPATH:" != *":$(brew --prefix)/share/zsh-completions:"* ]]; then
+        FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+    fi
+fi
+
+if [[ ":$FPATH:" != *":$HOME/.zfunc:"* ]]; then
+    FPATH=$HOME/.zfunc:$FPATH
+fi
