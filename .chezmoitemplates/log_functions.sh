c3eec8769a17e9f4c5b08712c243e705a5f0c98b
takanao <takanao14@gmail.com>
2026-05-21 10:37:49 +0900

    refactor: extract logging and GUI check functions into separate templates

---
+++ .chezmoitemplates/log_functions.sh
@@ -0,0 +1,17 @@
+# Color output
+readonly RED='\033[0;31m'
+readonly GREEN='\033[0;32m'
+readonly YELLOW='\033[1;33m'
+readonly NC='\033[0m'
+
+log_info() {
+    echo -e "${GREEN}[INFO]${NC} $*"
+}
+
+log_warn() {
+    echo -e "${YELLOW}[WARN]${NC} $*"
+}
+
+log_error() {
+    echo -e "${RED}[ERROR]${NC} $*"
+}
