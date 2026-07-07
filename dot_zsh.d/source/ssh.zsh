8f5e8b468f8b2ccfd5c19c5ca13374116ecca16a
takanao <takanao14@gmail.com>
2026-07-07 22:35:17 +0900

    feat: extend ssh function to support xterm-ghostty terminal

--- dot_zsh.d/source/ssh.zsh
+++ dot_zsh.d/source/ssh.zsh
@@ -1,5 +1,5 @@
 ssh() {
-  if [[ "${TERM:-}" == "xterm-kitty" ]]; then
+  if [[ "${TERM:-}" == "xterm-kitty" || "${TERM:-}" == "xterm-ghostty" ]]; then
     TERM=xterm-256color command ssh "$@"
   else
     command ssh "$@"
