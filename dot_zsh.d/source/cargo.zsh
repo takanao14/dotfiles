c68905072101fc530318a119ddb8838e25301837
takanao <takanao14@gmail.com>
2026-09-06 20:08:55 +0900

    feat: prepend rustup bin directory to PATH in zsh configuration for Homebrew setups

--- dot_zsh.d/source/cargo.zsh
+++ dot_zsh.d/source/cargo.zsh
@@ -4,4 +4,12 @@ if [[ -d "$_cargo_home/bin" && ":$PATH:" != *":$_cargo_home/bin:"* ]]; then
   export PATH="$_cargo_home/bin:$PATH"
 fi
 
-unset _cargo_home
+# Homebrew ships rustup keg-only, so its cargo/rustc proxies stay out of the
+# main bin directory. Prepend them ahead of any stale proxies in CARGO_HOME.
+_rustup_bin="${HOMEBREW_PREFIX:-/opt/homebrew}/opt/rustup/bin"
+
+if [[ -d "$_rustup_bin" && ":$PATH:" != *":$_rustup_bin:"* ]]; then
+  export PATH="$_rustup_bin:$PATH"
+fi
+
+unset _cargo_home _rustup_bin
